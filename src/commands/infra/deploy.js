d546fea0fc33175309e56874272f00ef09807dbd
Giuseppe Verni <gverni@twilio.com>
2021-03-07 19:46:10 +0000

    Create file if deploy success
    The file will contain the account sid of the project
    the resource(s) has been deployed to
    
    The file will be deleted when running `destory`

--- src/commands/infra/deploy.js
+++ src/commands/infra/deploy.js
@@ -1,16 +1,27 @@
 const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
+const { TwilioCliError } = require('@twilio/cli-core').services.error;
+
+const fs = require('fs');
 
 const { runPulumiCommand } = require('../../utils');
 
-class FunctionsDeploy extends TwilioClientCommand {
+class InfraDeploy extends TwilioClientCommand {
   async run() {
     await super.run();
-
     await runPulumiCommand(['up'], true, this.twilioClient);
+    try {
+      // Store account SID of the project used for deployment
+      fs.writeFileSync(
+        '.twilio-deploy',
+        JSON.stringify({ accountSid: this.twilioClient.accountSid })
+      );
+    } catch (error) {
+      throw new TwilioCliError('Error running destroy: ' + error.message);
+    }
   }
 }
 
-FunctionsDeploy.description =
+InfraDeploy.description =
   'Deploys and updates resources described in this directory to a Twilio project';
 
-module.exports = FunctionsDeploy;
+module.exports = InfraDeploy;
