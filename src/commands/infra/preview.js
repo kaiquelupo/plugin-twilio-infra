34cdc93459d6d43fd06ccbc8497c98d40f6339e3
Giuseppe Verni <gverni@twilio.com>
2021-03-07 19:48:28 +0000

    Change class name to name convention

--- src/commands/infra/preview.js
+++ src/commands/infra/preview.js
@@ -2,14 +2,14 @@ const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
 
 const { runPulumiCommand } = require('../../utils');
 
-class FunctionsPreview extends TwilioClientCommand {
+class InfraPreview extends TwilioClientCommand {
   async run() {
     await super.run();
     await runPulumiCommand(['preview'], true);
   }
 }
 
-FunctionsPreview.description =
+InfraPreview.description =
   'Previews changes related to resources described in this directory and mapped to a Twilio project';
 
-module.exports = FunctionsPreview;
+module.exports = InfraPreview;
