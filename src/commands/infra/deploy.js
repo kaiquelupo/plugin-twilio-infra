72f94a7d1da96ddfe1d89bdaca3a345df5a6244e
Giuseppe Verni <gverni@twilio.com>
2021-04-01 14:14:06 +0100

    Use new helpers in `infra:deploy`

--- src/commands/infra/deploy.js
+++ src/commands/infra/deploy.js
@@ -1,20 +1,17 @@
 const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
 const { TwilioCliError } = require('@twilio/cli-core').services.error;
 
-const fs = require('fs');
+const { addDeployment } = require('../../deployments');
 
 const { runPulumiCommand } = require('../../utils');
 
 class InfraDeploy extends TwilioClientCommand {
   async run() {
     await super.run();
-    await runPulumiCommand(['up'], true, this.twilioClient);
+    runPulumiCommand(['up'], true, this.twilioClient);
     try {
       // Store account SID of the project used for deployment
-      fs.writeFileSync(
-        '.twilio-deploy',
-        JSON.stringify({ accountSid: this.twilioClient.accountSid })
-      );
+      addDeployment(this.twilioClient.accountSid);
     } catch (error) {
       throw new TwilioCliError('Error running destroy: ' + error.message);
     }
