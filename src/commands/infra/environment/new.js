4dce6b31778643ec2bd8d05772b93c4998b32cff
Giuseppe Verni <gverni@twilio.com>
2021-04-14 14:04:03 +0100

    Implement `infra:environment:new`

---
+++ src/commands/infra/environment/new.js
@@ -0,0 +1,50 @@
+const { TwilioClientCommand } = require('@twilio/cli-core').baseCommands;
+const { TwilioCliError } = require('@twilio/cli-core').services.error;
+
+const { readInfra, addInfra } = require('../../../infra');
+const { getPulumiStack, Printer, runPulumiCommand } = require('../../../utils');
+
+class InfraEnvironmentNew extends TwilioClientCommand {
+  async run() {
+    await super.run();
+    try {
+      let { args } = this.parse(InfraEnvironmentNew);
+      let accountSid = this.twilioClient.accountSid;
+      let deploymentEnvironments = readInfra();
+
+      if (
+        deploymentEnvironments[accountSid] &&
+        deploymentEnvironments[accountSid].deployed
+      ) {
+        Printer.print(
+          `Your Twilio project ${accountSid} is already associated to another environment (${deploymentEnvironments[accountSid].environment})\n\nUse\n\n  $ twilio profiles:use <profile name>\n\nto select a different project, or:\n\n  $ twilio profiles:create\n\nto create a new profile.`
+        );
+      } else {
+        runPulumiCommand(['stack', 'init', args.environmentName]);
+        if (this.twilioClient) {
+          addInfra(accountSid, getPulumiStack());
+        }
+        Printer.printSuccess(
+          'New environment created. When you are ready, use\n\n  $ twilio infra:deploy\n\nto deploy it to your Twilio prpject'
+        );
+      }
+    } catch (error) {
+      throw new TwilioCliError(
+        'Error running `environment:get`: ' + error.message
+      );
+    }
+  }
+}
+
+InfraEnvironmentNew.description =
+  'Get the deployment environment set for the current project';
+
+InfraEnvironmentNew.args = [
+  {
+    name: 'environmentName',
+    required: true,
+    description: 'The name of the environment to create',
+  },
+];
+
+module.exports = InfraEnvironmentNew;
